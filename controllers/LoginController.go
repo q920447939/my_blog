839a819df37752aaee460689efb7372bd6f7c38d
lim <lim@haokukeji.com>
2018-11-06 23:13:09 +0800

    fox merge error

--- controllers/LoginController.go
+++ controllers/LoginController.go
@@ -1,29 +1,39 @@
 package controllers
 
 import (
-		"github.com/astaxie/beego"
 	"fmt"
+	"github.com/astaxie/beego"
 )
 
 type LoginController struct {
-	beego.Controller
+	BaseResultController
 }
 
 func (this *LoginController) Get() {
+	if this.Input().Get("exit")  =="true" {
+		this.TplName = "index.html"
+		//设置cookie
+		this.Ctx.SetCookie("username", "", fmt.Sprint(beego.AppConfig.String("COOKIE::MAX_TIME")), "/")
+		this.Ctx.SetCookie("password", "", fmt.Sprint(beego.AppConfig.String("COOKIE::EXPIRE_TIME")), "/")
+		this.Data["isIndex"] = true
+
+		return
+	}
 	this.TplName = "login.html"
 }
 
-func (this *LoginController)  Post() {
+func (this *LoginController) Post() {
+	this.TplName = "demo.html"
 	username := this.Input().Get("username")
 	password := this.Input().Get("password")
 	if username == "" || password == "" {
-		controllers.BaseResultController{}.JsonResult(4000,"账号或密码为空")
+		this.JsonResult(4000,"账号或密码为空")
 		return
 	}
 	cUsername := beego.AppConfig.String("username")
 	cPassword := beego.AppConfig.String("password")
 	if username != cUsername || password != cPassword {
-		controllers.BaseResultController{}.JsonResult(4000,"账号或密码错误")
+		this.JsonResult(4000,"账号或密码错误")
 		return
 	}
 	//设置cookie
--- controllers/default.go
+++ controllers/default.go
@@ -1,9 +1,8 @@
 package controllers
 
 import (
-	"fmt"
-	"github.com/astaxie/beego"
-	"my_blog/connection/mysql"
+		"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 type MainController struct {
@@ -11,9 +10,30 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	db := mysql.Db.Unscoped().Table("test")
-	fmt.Println(db)
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["isIndex"] = true
+	c.Data["isLogin"] = checkAccount(c.Ctx)
 	c.TplName = "index.html"
 }
+
+
+
+
+func checkAccount(ctx *context.Context) bool {
+	ck, err := ctx.Request.Cookie("username")
+	if err != nil {
+		return false
+	}
+
+	uname := ck.Value
+
+	ck, err = ctx.Request.Cookie("password")
+	if err != nil {
+		return false
+	}
+
+	pwd := ck.Value
+	return uname == beego.AppConfig.String("username") &&
+		pwd == beego.AppConfig.String("password")
+}
+
+
